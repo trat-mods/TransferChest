97ba202bc1abedbb2435cad019fa847173c8b512
Matteo Beltrame <bltmtt@gmail.com>
2020-12-09 13:38:42 +0100

    Idea

---
+++ src/main/java/net/enderbox/mod/loader/EBLoader.java
@@ -0,0 +1,11 @@
+package net.enderbox.mod.loader;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class EBLoader implements ModInitializer
+{
+    @Override public void onInitialize()
+    {
+    
+    }
+}
